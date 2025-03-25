96c241aea2fd3c632b199549fa3756b264e866fc
Andres Blanco Bonilla <afbb@princeton.edu>
2025-03-24 23:34:15 -0400

    state map with outline working, need to modularize a bit more

--- route_test.py
+++ route_test.py
@@ -15,8 +15,16 @@ app = flask.Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
-def index():
+@app.route('/route', methods=['GET'])
+def route():
 
     html_code = flask.render_template('route_test.html')
+    response = flask.make_response(html_code)
+    return response
+
+@app.route('/nj', methods=['GET'])
+def nj_map():
+
+    html_code = flask.render_template('nj_map.html')
     response = flask.make_response(html_code)
     return response
